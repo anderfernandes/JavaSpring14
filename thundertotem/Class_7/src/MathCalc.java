be729416ce5eb64086e0a5a9040324eaf2196704
John Prymuszewski <grimmhoof@hotmail.com>
2014-06-04 17:48:03 -0500

    More Function stuff
    Playing around with Switch Statements

--- thundertotem/Class_7/src/MathCalc.java
+++ thundertotem/Class_7/src/MathCalc.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
-public class EvilMath{
+public class MathCalc{
 	public static void main (String[] Args) {
 		
 		Scanner sc = new Scanner(System.in);
-		System.out.println("This is a test about functions")
+		System.out.println("This is a test about functions");
 		System.out.print("Enter number 1: ");
 		int num1 = sc.nextInt();
 		System.out.print("Enter Number 2: ");
@@ -25,9 +25,9 @@ public class EvilMath{
 				break;
 			case 4 :
 				System.out.println("The value of " + num1 + " - " + num2 + " is " + subtract(num1, num2));
-				break
+				break;
 			default:
-				System.out.print("Error, Enter a number for a calculation. /n 1 is Multiply /n 2 is divide /n 3 is add /n 4 is subtract")
+				System.out.print("Error, Enter a number for a calculation. /n 1 is Multiply /n 2 is divide /n 3 is add /n 4 is subtract");
 		}
 		//System.out.println("The value of " + num1 + " X " + num2 + " is " + multiply(num1, num2));
 		//System.out.println("The value of " + num1 + " / " + num2 + " is " + divide(num1, num2));
@@ -47,7 +47,7 @@ public class EvilMath{
 		int result = (a + b) ;
 		return result;	
 	}
-	public static int subtract(int i1, int i2){
+	public static int subtract(int a, int b){
 		int result = (a - b) ;
 		return result;	
 	}
