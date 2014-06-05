8949069c403bf3b9b3d2aa13e3a5340d4e03a761
John Prymuszewski <grimmhoof@hotmail.com>
2014-06-04 20:05:51 -0500

    More analize numbers

--- thundertotem/Class_7/src/AnalizeNumbersTwo.java
+++ thundertotem/Class_7/src/AnalizeNumbersTwo.java
@@ -1,18 +1,21 @@
 import java.util.Scanner;
 
-public class AnalizeNumbers {
+public class AnalizeNumbersTwo {
 	public static void main (String[] Args){
-		final int NUMBER_OF_ELEMENTS = 10;
+		final int NUMBER_OF_ELEMENTS = 5;
 		double[] numbers = new double[NUMBER_OF_ELEMENTS];
 		double sum = 0;
 		
 		Scanner sc = new Scanner(System.in);
 		
 		for (int i = 0; i < NUMBER_OF_ELEMENTS; i++){
-			System.out.print("Enter a number: ");
-			numbers[i] = sc.nextDouble();
-			sum += numbers[i];
+			//System.out.print("Enter a number: ");
+			//numbers[i] = sc.nextDouble();
+			//System.out.println(numbers[i]);
+			//sum += numbers[i];
+			numbers[i] = (double)(Math.random() * 100);
 		}
+		
 		
 		double average = sum / NUMBER_OF_ELEMENTS;
 		
--- thundertotem/Class_7/src/CopyArray.java
+++ thundertotem/Class_7/src/CopyArray.java
@@ -1,18 +1,20 @@
 import java.util.Scanner;
 
-public class AnalizeNumbers {
+public class CopyArray {
 	public static void main (String[] Args){
-		final int NUMBER_OF_ELEMENTS = 10;
-		double[] numbers = new double[NUMBER_OF_ELEMENTS];
-		double sum = 0;
+		final int NUMBER_OF_ELEMENTS = 5;
+		int[] numbers = new int[NUMBER_OF_ELEMENTS];
+		int sum = 0;
 		
 		Scanner sc = new Scanner(System.in);
 		
 		for (int i = 0; i < NUMBER_OF_ELEMENTS; i++){
-			System.out.print("Enter a number: ");
-			numbers[i] = sc.nextDouble();
-			sum += numbers[i];
+			numbers[i] = (int)(Math.random() * 100);
 		}
+		/*for (int i = 0; i < numebrs.length; i++){
+			System.out.print("");
+		}*/
+		
 		
 		double average = sum / NUMBER_OF_ELEMENTS;
 		
